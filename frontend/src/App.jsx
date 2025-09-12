90bdc6b984788f3105726b6e1d606f864e3dd8b1
Naveen pandian <npandian515@gmail.com>
2025-09-12 12:56:06 +0530

    feat: implement user authentication flow with Privy integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-
+import {usePrivy} from "@privy-io/react-auth"
 
 import './App.css'
 
 function App() {
+  const {ready,authenticated,user,login,logout} = usePrivy()
+
+  if (!ready) {
+    return <div>Loading Privy...</div>;
+  }
+
+  if (!authenticated) {
+    return (
+      <button onClick={() => login()}>Login</button>
+    );
+  }
 
   return (
-    <>
-      
-    </>
+    <div>
+      <p>Welcome, {user?.id}</p>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+    
   )
 }
 
