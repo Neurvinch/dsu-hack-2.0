cff9e315281bcc86bba0953e859b653402973135
Naveen pandian <npandian515@gmail.com>
2025-09-12 12:47:10 +0530

    feat: add Privy authentication provider to the frontend
    - Updated package.json to include @privy-io/react-auth dependency (version 2.24.0).
    - Wrapped the main application in PrivyProvider in main.jsx to enable authentication features.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {PrivyProvider} from "@privy-io/react-auth"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PrivyProvider>
+      <App />
+    </PrivyProvider>
+    
   </StrictMode>,
 )
